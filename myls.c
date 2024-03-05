7ee346dc8313436c25c24e7c99325a1b57d85413
Ines <inguipe@gmail.com>
2024-03-05 21:45:22 +0100

    finish myls

--- myls.c
+++ myls.c
@@ -21,21 +21,21 @@ int gerror(int nParameter, char *Parameter[])
 
 		// In case that we have enough arguments, but still there is an error
 		if (nParameter > 0)
-                {
+		{
 			// We read each parameter and check which is the error
-                        for (int i = 0; i < nParameter; i++)
-                        {
-        	                if (i > 0) size += sprintf(format + size, " %s", Parameter[i]);
-	                        else size = sprintf(format,"%s", Parameter[i]);
-                	        if (i+1 < nParameter) size += sprintf(format + size, "%c", ':');
-                        }
-                }
+			for (int i = 0; i < nParameter; i++)
+			{
+				if (i > 0) size += sprintf(format + size, " %s", Parameter[i]);
+				else size = sprintf(format,"%s", Parameter[i]);
+				if (i+1 < nParameter) size += sprintf(format + size, "%c", ':');
+			}
+		}
 		
 		// Print the error and return -1
-                if (nParameter > 0) printf("%s: %s\n", format, strerror(errno));
-                else printf("%s\n", strerror(errno));
-                return -1;
-        }
+		if (nParameter > 0) printf("%s: %s\n", format, strerror(errno));
+		else printf("%s\n", strerror(errno));
+		return -1;
+    }
 
 	// If there is no error, finish
     return 0;
@@ -44,47 +44,49 @@ int gerror(int nParameter, char *Parameter[])
 int main(int argc, char *argv[]) {
     DIR *dir;
 
-    // data structure returned by readdir
+    // Data structure returned by function readdir
     struct dirent *entry;
     char actual_path[PATH_MAX] = {0};
 
-    // There has been no argument passed
+    // If there has been no argument passed, return error
     if (argc < 2 ) 
     {
-        if (getcwd(actual_path, sizeof(actual_path)) == NULL) {
-            return gerror(argc, argv);
-        }
+        if (getcwd(actual_path, sizeof(actual_path)) == NULL)
+		{
+			return gerror(argc, argv);
+		}
 	
     }
     // Copy the argument passed to the local variable 
-    else  strcpy(actual_path, argv[1]);
+    else strcpy(actual_path, argv[1]);
     
-
     // Open the directory
-    if ((dir = opendir(actual_path)) == NULL) 
+    if ((dir = opendir(actual_path)) == NULL)
     {
-    	// we format the error to return the same messages as ls command
-	char erro[PATH_MAX+25]={0};
-	sprintf(erro,"cannot access '%s'", argv[1]);
+    	// We format the error to return the same messages as ls command
+		char erro[PATH_MAX + 25] = {0};
+		sprintf(erro,"cannot access '%s'", argv[1]);
 	
-	// assign in the name file passed as argument the message we want to return
-	argv[1]=erro;
+		// assign in the name file passed as argument the message we want to return
+		argv[1] = erro;
 	
-	// Use function gerror and finish
+		// Use function gerror and finish
         return gerror(argc, (char **)argv);
     }
 
     // Read and print directory entries
-    while ((entry = readdir(dir)) != NULL) {
+    while ((entry = readdir(dir)) != NULL) 
+	{
         printf("%s\n", entry->d_name);
     }
 
     // If we cannot close the directory, return error
-    if ( closedir(dir) == -1)
-     {
-        return gerror(-1, (char **)"closedir");
-     }
+    if (closedir(dir) == -1) 
+	{
+		return gerror(-1, (char **)"closedir");
+	}
 	
+	// Finish
     return 0;
 }
 
